dde3b5cce27967bc666839a8e6eb410d95724cf7
Jay Singh <jay@Jays-MacBook-Pro.local>
2023-10-24 22:15:24 +0100

    milestone_4.py taks 1 - 4 completed
    Created a class called Hangman with attributes initialised.
    
    function check_letter created which checks the guess the user has made, if the guess is in the word then it will print a message and also replaces the '_' in the word_guessed list with the correct letter. whilst decrementing num_letters by 1.
    
    function ask_for_input created which iteratively asks the user for a letter until valid input. upon valid input, the check_letter function is called.

---
+++ hangman/milestone_4.py
@@ -0,0 +1,44 @@
+#%%
+import random
+import milestone_3
+
+class Hangman():
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in range(len(self.word))]
+        self.list_of_guesses = []
+        self.num_letters = set(self.word)
+        print(f"The mystery word has {new_game.num_letters} characters")
+        print(f"{new_game.word_guessed}")
+    
+    def check_letter(self, letter):
+        if letter in self.word:
+            print(f"Good guess! {letter} is in the word")
+            for i, letter_in_word in enumerate(self.word):
+                if letter_in_word == letter:
+                    self.word_guessed[i] = letter
+            self.num_letters -= 1
+        else: 
+            self.num_lives -= 1 
+            print(f"Sorry, {letter} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+    
+    def ask_for_input(self):
+        while True:
+            letter = input("Guess a letter: ").lower()
+            if len(letter) == 1 and letter.isalpha():
+                if letter in self.list_of_guesses:
+                    print(f"You have already guessed {letter}, try a different letter...")
+                else:
+                    self.list_of_guesses.append(letter)
+                    self.check_letter(letter, self.word_guessed)
+            else:
+                print("Invalid input. Please enter a single alphabetical character.")
+            
+                
+new_game = Hangman()
+new_game.ask_for_input()
+
+# %%
